4d55c97ba7b1bffb1d3ab95237a773da89c78332
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-05-30 04:12:52 +0600

    var can be reassigned val cannot be resigned.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
-    println("Hello wrold")
+    println("Hello World")
+    println("I learning kotlin")
+    println("It is awesome.")
+    println(3+3)
+    print("It is doesn't print new line")//new line not print
+    println("Ok")
+    //var and val can be declare variable and value
+    var name = "Anawar"
+    val birthYear = 1988
+    println(name + " " + birthYear)
+    //    var also: String = "John".also { name = it } // var variable can be modified/changeable
+    name = "John" // val can not be resigned
+    println(name + " " + birthYear)
+
+    // When I specify a type
+    var person: String
+    person = "John"
+    println(person)
 }
